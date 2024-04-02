c4d9160fc53ecae191dd5fd746860996042c0774
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-04-02 11:55:54 +0700

    Menambahkan display Mahasiswa

--- Contoh 3/Contoh 3.cpp
+++ Contoh 3/Contoh 3.cpp
@@ -12,3 +12,24 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
+	cout << "alamat mahasiswa : " << endl;
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Umur mahasiswa : ";
+	cin >> mhs.umur;
+
+
+}
+
+
+
